3ea0af833b59f4d24d459e345b448f27f0adadab
Mohammed-Chakib Belgaid <mohammed-chakib.belgaid@univ-lille.fr>
2020-05-07 13:44:42 +0200

    add measureit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # energy-scripts
 A repository of scripts that can be used to monitor energy consumption.
+
+
+# Requirement 
+
+- time (for the verbose mode)
+
+    apt install time 
+
+# How to use 
+
+    ./measureit.sh cmd
+
+for more details, sue the verbose mode with the option ```-v``` 
+    
+    ./measureit.sh -v cmd 
---
+++ measureit.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+
+
+PKG0='/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/energy_uj'
+# DRAM0='/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/intel-rapl:0:0/energy_uj'
+# PKG1='/sys/devices/virtual/powercap/intel-rapl/intel-rapl:1/energy_uj'
+# DRAM1='/sys/devices/virtual/powercap/intel-rapl/intel-rapl:1/intel-rapl:1:0/energy_uj'
+
+while getopts "v" o; do
+    case "${o}" in
+        v) 
+            verbose="True"
+        ;;   
+        esac
+done
+shift $((OPTIND-1))
+
+
+beginT=` date +"%s%N"`
+beginPKG0=` cat $PKG0`
+# beginDRAM0=` cat $DRAM0`
+# beginPKG1=` cat $PKG1`
+# beginDRAM1=` cat $DRAM1`
+if [[ -n $verbose ]] 
+then 
+/usr/bin/time -apv $@
+else
+$@
+fi
+
+endT=` date +"%s%N"`
+endPKG0=` cat $PKG0`
+# endDRAM0=` cat $DRAM0`
+# endPKG1=` cat $PKG1`
+# endDRAM1=` cat $DRAM1`
+
+duration=$((($endT - $beginT)/1000000))
+
+pkg0=$((($endPKG0-$beginPKG0)/1000))
+# pkg1=$((($endPKG1-$beginPKG1)/1000))
+# dram0=$((($endDRAM0-$beginDRAM0)/1000))
+# dram1=$((($endDRAM1-$beginDRAM1)/1000))
+
+# pkg=$(($pkg0+$pkg1))
+# dram=$(($dram0+$dram1))
+
+# echo 'duration (ms)'   $duration
+if [[ -z $verbose ]] 
+then
+    echo '      duration (ms):'   $duration 
+fi 
+
+echo '      energy (mJ):'        $pkg0
+# echo dram       $dram
