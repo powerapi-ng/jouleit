b58cd251a69c1fe22076a873db5827f480129bcf
Mohammed-Chakib Belgaid <mohammed-chakib.belgaid@univ-lille.fr>
2021-04-07 15:18:11 +0200

    add : verbose mode

--- measureit.sh
+++ measureit.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+while getopts "v" o; do
+    case "${o}" in
+    v)
+        verbose="True"
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 ######### generic functions
 read_energy() {
     dir=$@
@@ -39,11 +48,29 @@ calculate_energy() {
 '
 }
 
+calculate_energy_verbose() {
+    begins=$1
+    ends=$2
+    maxenergies=$3
+    echo | awk -v ends=$ends -v begins=$begins -v maxenergies=$MAXPKG 'BEGIN \
+{   \
+    split(ends,ends1,","); \
+    split(begins,begins1,","); \
+    split(maxenergies,maxenergies1,","); \
+    for (i in ends1 ) {x= ends1[i] - begins1[i] ;\
+        if (x < 0) {x=x+maxenergies1[i] } ;\
+        printf x "\n" ; \
+    }\
+} \
+'
+}
+
 ############### get the paths of each device #######################
 
 x=$(find /sys/devices/virtual/powercap/intel-rapl -name "name")
 drams=""
 cores=""
+uncores=""
 for i in ${x[@]}; do
     s=$(cat $i)
     case $s in
@@ -52,6 +79,9 @@ for i in ${x[@]}; do
         ;;
     core)
         cores+=${i%name}" "
+        ;;
+    uncore)
+        uncores+=${i%name}" "
         ;;
     esac
 done
@@ -66,7 +96,11 @@ if [ -n "$drams" ]; then
 fi
 
 if [ -n "$cores" ]; then
-    MAXCORES=$(read_maxenergy $cores)
+    MAXCORE=$(read_maxenergy $cores)
+fi
+
+if [ -n "$uncores" ]; then
+    MAXUNCORE=$(read_maxenergy $uncores)
 fi
 
 ############# get the  energy  before the begining of the command for each device #################
@@ -80,6 +114,11 @@ fi
 if [ -n "$cores" ]; then
     begincore=$(read_energy $cores)
 fi
+
+if [ -n "$uncores" ]; then
+    beginuncore=$(read_energy $uncores)
+fi
+
 beginT=$(date +"%s%N")
 ########################## the command
 $@
@@ -97,14 +136,17 @@ if [ -n "$cores" ]; then
     endcore=$(read_energy $cores)
 fi
 
+if [ -n "$uncores" ]; then
+    enduncore=$(read_energy $uncores)
+fi
+
 ## calculate the differences
 
-duration=$((($endT - $beginT) / 1000000))
+duration=$((($endT - $beginT) / 1000))
 
 enegypkg=$(calculate_energy $beginpkg $endpkg $MAXPKG)
 
 if [ -n "$drams" ]; then
-
     energydram=$(calculate_energy $begindram $enddram $MAXDRAM)
 
 fi
@@ -114,19 +156,104 @@ if [ -n "$cores" ]; then
 
 fi
 
-#### printing the results
-printf "execution time  : %10d ms\n" $duration
+if [ -n "$uncores" ]; then
+    energyuncore=$(calculate_energy $beginuncore $enduncore $MAXUNCORE)
 
-printf 'energy cpu      : %10d uj\n' $enegypkg
+fi
+
+#### verbose mode
+if [ -n "$verbose" ]; then
+    pkgs=$(calculate_energy_verbose $beginpkg $endpkg $MAXPKG)
+    pkgs=${pkgs%'0'}
+
+    cpu=0
+    for i in ${pkgs[@]}; do
+        cpu=$((cpu + 1))
+    done
+    printf "\n"
+
+    echo -n ' ---------------'
+    for i in $(seq 1 $cpu); do
+        echo -n '------------------'
+    done
+    printf "\n"
+
+    printf "| Cpu name      | "
+    x=$((cpu - 1))
+    for i in $(seq 0 $x); do
+        printf "%-15s | " "cpu $i"
+    done
+    printf "\n"
+
+    echo -n ' ---------------'
+    for i in $(seq 1 $cpu); do
+        echo -n '------------------'
+    done
+    printf "\n"
+
+    pkgs=${pkgs%'0'}
+    printf "| Energy cpu    | "
+
+    for i in ${pkgs[@]}; do
+        printf "%-15d | " $i
+    done
+    printf "\n"
+
+    if [ -n "$drams" ]; then
+        printf "| Energy dram   | "
+        drams=$(calculate_energy_verbose $begindram $enddram $MAXDRAM)
+        drams=${drams%'0'}
+        for i in ${drams[@]}; do
+            printf "%-15d | " $i
+        done
+        printf "\n"
+    fi
+
+    if [ -n "$cores" ]; then
+        printf "| Energy core   | "
+        cores=$(calculate_energy_verbose $begincore $endcore $MAXCORE)
+        cores=${cores%'0'}
+        for i in ${cores[@]}; do
+            printf "%-15d | " $i
+        done
+        printf "\n"
+    fi
+
+    if [ -n "$uncores" ]; then
+        printf "| Energy uncore | "
+        uncores=$(calculate_energy_verbose $beginuncore $enduncore $MAXUNCORE)
+        uncores=${uncores%'0'}
+        for i in ${uncores[@]}; do
+            printf "%-15d | " $i
+        done
+        printf "\n"
+    fi
+    echo -n ' ---------------'
+    for i in $(seq 1 $cpu); do
+        echo -n '------------------'
+    done
+    printf "\n\n"
+fi
+
+############ all results
+echo "Global measures"
+printf "Execution time    : %15d us\n" $duration
+
+printf 'Energy cpu        : %15d uj\n' $enegypkg
 
 if [ -n "$drams" ]; then
 
-    printf 'energy dram     : %10d uj\n' $energydram
+    printf 'Energy dram       : %15d uj\n' $energydram
 
 fi
 
 if [ -n "$cores" ]; then
 
-    printf 'energy core-cpu : %10d uj\n' $energycore
+    printf 'Energy core-cpu   : %15d uj\n' $energycore
+
+fi
+if [ -n "$uncores" ]; then
+
+    printf 'Energy uncore-cpu : %15d uj\n' $energyuncore
 
 fi
