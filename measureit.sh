a6f5e318672285ed1a71831f48d5268a072794d7
Mohammed-Chakib Belgaid <mohammed-chakib.belgaid@univ-lille.fr>
2021-04-09 23:50:06 +0200

    fix: bug and imporved the code

--- measureit.sh
+++ measureit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# the option -v
 while getopts "v" o; do
     case "${o}" in
     v)
@@ -7,253 +8,163 @@ while getopts "v" o; do
         ;;
     esac
 done
+
 shift $((OPTIND - 1))
 
-######### generic functions
+# Read data
+
 read_energy() {
-    dir=$@
-    energy=""
-    for i in ${dir[@]}; do
-        s=$(cat $i/energy_uj)
-        energy=$energy$s","
+    components=$(find /sys/devices/virtual/powercap/intel-rapl -name "energy_uj")
+
+    data=""
+    for component in ${components[@]}; do
+
+        name=$(cat ${component%energy_uj}/name)
+        energy=$(cat $component)
+        data=$data$component,$name,$energy\;
     done
-    echo $energy
+    data="${data%;}"
+    echo $data
 }
 
 read_maxenergy() {
-    dir=$@
-    energy=""
-    for i in ${dir[@]}; do
-        s=$(cat $i/max_energy_range_uj)
-        energy=$energy$s","
+    components=$(find /sys/devices/virtual/powercap/intel-rapl -name "energy_uj")
+
+    data=""
+    for component in ${components[@]}; do
+
+        name=$(cat ${component%energy_uj}/name)
+        energy=$(cat ${component%energy_uj}/max_energy_range_uj)
+        data=$data$component,$name,$energy\;
     done
-    echo $energy
+    data="${data%;}"
+    echo $data
 }
+
+# Calculate the energyies
 
 calculate_energy() {
     begins=$1
     ends=$2
     maxenergies=$3
-    echo | awk -v ends=$ends -v begins=$begins -v maxenergies=$MAXPKG 'BEGIN \
-{   energiy=0;\
-    split(ends,ends1,","); \
-    split(begins,begins1,","); \
-    split(maxenergies,maxenergies1,","); \
-    for (i in ends1 ) {x= ends1[i] - begins1[i] ;\
-        if (x < 0) {x=x+maxenergies1[i] } ;\
-        energy += x ; \
-    }\
-    printf energy "\n"; \
-} \
-'
+    echo | awk -v begins=$begins -v ends=$ends -v maxenergies=$maxenergies 'BEGIN \
+    {
+    split(ends,ends1,";");
+    split(begins,begins1,";");
+    split(maxenergies,maxenergies1,";");
+
+    for (i in ends1 ){
+
+        split(ends1[i],dataends,",")
+        split(begins1[i],databegins,",")
+        split(maxenergies1[i],datamaxenergies,",")
+        x = dataends[3] - databegins[3]
+        if (x < 0 )
+            {
+                x=x+datamaxenergies[3]
+            }
+        printf dataends[1]","dataends[2]","x";"
+        }
+
+    }'
+
 }
 
-calculate_energy_verbose() {
-    begins=$1
-    ends=$2
-    maxenergies=$3
-    echo | awk -v ends=$ends -v begins=$begins -v maxenergies=$MAXPKG 'BEGIN \
-{   \
-    split(ends,ends1,","); \
-    split(begins,begins1,","); \
-    split(maxenergies,maxenergies1,","); \
-    for (i in ends1 ) {x= ends1[i] - begins1[i] ;\
-        if (x < 0) {x=x+maxenergies1[i] } ;\
-        printf x "\n" ; \
-    }\
-} \
-'
+#Printing functions
+
+print_time() {
+    beginT=$1
+    endT=$2
+    duration=$((($endT - $beginT) / 1000))
+
+    echo " ------------------------------------------- "
+    echo "|             execution time  (us)          |"
+    echo " ------------------------------------------- "
+    printf "|             %-30d|\n" $duration
+    echo " ------------------------------------------- "
+
 }
 
-############### get the paths of each device #######################
+print_header() {
 
-x=$(find /sys/devices/virtual/powercap/intel-rapl -name "name")
-drams=""
-cores=""
-uncores=""
-for i in ${x[@]}; do
-    s=$(cat $i)
-    case $s in
-    dram)
-        drams+=${i%name}" "
-        ;;
-    core)
-        cores+=${i%name}" "
-        ;;
-    uncore)
-        uncores+=${i%name}" "
-        ;;
-    esac
-done
+    printf "| CPU    | %-10s | %-19s |\n" "Component" "energy (uJ)"
+    echo " ------------------------------------------- "
+}
 
-pkgs=$(find /sys/devices/virtual/powercap/intel-rapl/ -name intel-rapl\:[[:digit:]])
+print_details() {
 
-############# get the max energy window for each device #################
-MAXPKG=$(read_maxenergy $pkgs)
+    echo | awk -v data=$1 'BEGIN \
+    {
+        split(data,data1,";");
+        for (line in data1 )  {
+            split(data1[line],line1,",");
+            path=line1[1];
+            name=line1[2];
+            value=line1[3];
+            split(path,path1,":")
+            cpu=path1[2]
+            split(cpu,cpu1,'//')
+            cpu=cpu1[1]
+            if (match(name, "package")) { name = "cpu" }
+            name =toupper(name)
+            printf "| CPU-%-3d| %-10s | %-19d |\n" ,cpu,name,value
+        }
+    }'
+    echo " ------------------------------------------- "
+}
 
-if [ -n "$drams" ]; then
-    MAXDRAM=$(read_maxenergy $drams)
-fi
+print_global() {
 
-if [ -n "$cores" ]; then
-    MAXCORE=$(read_maxenergy $cores)
-fi
+    echo | awk -v data=$1 'BEGIN \
+    {
+        
+        split(data,data1,";");
+        for (line in data1 )  {
+            split(data1[line],line1,",");
+            path=line1[1];
+            name=line1[2];
+            value=line1[3];
+            split(path,path1,":")
+            cpu=path1[2]
+            split(cpu,cpu1,'//')
+            cpu=cpu1[1]
+            energies[name]=energies[name]+value
+        }
+        for (i in energies ) {
+            # printf "global %s %d \n" ,toupper(i),energies[i] 
+            printf "| Global | %-10s | %-19d |\n" , toupper(i),energies[i]
+        }
+    }'
+    echo " ------------------------------------------- "
 
-if [ -n "$uncores" ]; then
-    MAXUNCORE=$(read_maxenergy $uncores)
-fi
+}
 
-############# get the  energy  before the begining of the command for each device #################
+###############################
+maxenergies=$(read_maxenergy)
 
-beginpkg=$(read_energy $pkgs)
-
-if [ -n "$drams" ]; then
-    begindram=$(read_energy $drams)
-fi
-
-if [ -n "$cores" ]; then
-    begincore=$(read_energy $cores)
-fi
-
-if [ -n "$uncores" ]; then
-    beginuncore=$(read_energy $uncores)
-fi
-
+begin_energy=$(read_energy)
 beginT=$(date +"%s%N")
-########################## the command
+
+###############################################
 $@
-
-############# get the  energy  before the machine after the end  of the command for each device #################
+###############################################
 endT=$(date +"%s%N")
+end_energy=$(read_energy)
 
-endpkg=$(read_energy $pkgs)
+### Calculate the energies
+energies=$(calculate_energy $begin_energy $end_energy $maxenergies)
+echo $energies
 
-if [ -n "$drams" ]; then
-    enddram=$(read_energy $drams)
-fi
+# Remove the last ;
+energies="${energies%;}"
+# Change package with CPU
+energies=$(echo $energies | sed -r 's/package-([0-9]+)/cpu/g')
 
-if [ -n "$cores" ]; then
-    endcore=$(read_energy $cores)
-fi
+## Visualisation
+print_time $beginT $endT
+print_header
+print_global $energies
 
-if [ -n "$uncores" ]; then
-    enduncore=$(read_energy $uncores)
-fi
-
-## calculate the differences
-
-duration=$((($endT - $beginT) / 1000))
-
-enegypkg=$(calculate_energy $beginpkg $endpkg $MAXPKG)
-
-if [ -n "$drams" ]; then
-    energydram=$(calculate_energy $begindram $enddram $MAXDRAM)
-
-fi
-
-if [ -n "$cores" ]; then
-    energycore=$(calculate_energy $begincore $endcore $MAXCORE)
-
-fi
-
-if [ -n "$uncores" ]; then
-    energyuncore=$(calculate_energy $beginuncore $enduncore $MAXUNCORE)
-
-fi
-
-#### verbose mode
 if [ -n "$verbose" ]; then
-    pkgs=$(calculate_energy_verbose $beginpkg $endpkg $MAXPKG)
-    pkgs=${pkgs%'0'}
-
-    cpu=0
-    for i in ${pkgs[@]}; do
-        cpu=$((cpu + 1))
-    done
-    printf "\n"
-
-    echo -n ' ---------------'
-    for i in $(seq 1 $cpu); do
-        echo -n '------------------'
-    done
-    printf "\n"
-
-    printf "| Cpu name      | "
-    x=$((cpu - 1))
-    for i in $(seq 0 $x); do
-        printf "%-15s | " "cpu $i"
-    done
-    printf "\n"
-
-    echo -n ' ---------------'
-    for i in $(seq 1 $cpu); do
-        echo -n '------------------'
-    done
-    printf "\n"
-
-    pkgs=${pkgs%'0'}
-    printf "| Energy cpu    | "
-
-    for i in ${pkgs[@]}; do
-        printf "%-15d | " $i
-    done
-    printf "\n"
-
-    if [ -n "$drams" ]; then
-        printf "| Energy dram   | "
-        drams=$(calculate_energy_verbose $begindram $enddram $MAXDRAM)
-        drams=${drams%'0'}
-        for i in ${drams[@]}; do
-            printf "%-15d | " $i
-        done
-        printf "\n"
-    fi
-
-    if [ -n "$cores" ]; then
-        printf "| Energy core   | "
-        cores=$(calculate_energy_verbose $begincore $endcore $MAXCORE)
-        cores=${cores%'0'}
-        for i in ${cores[@]}; do
-            printf "%-15d | " $i
-        done
-        printf "\n"
-    fi
-
-    if [ -n "$uncores" ]; then
-        printf "| Energy uncore | "
-        uncores=$(calculate_energy_verbose $beginuncore $enduncore $MAXUNCORE)
-        uncores=${uncores%'0'}
-        for i in ${uncores[@]}; do
-            printf "%-15d | " $i
-        done
-        printf "\n"
-    fi
-    echo -n ' ---------------'
-    for i in $(seq 1 $cpu); do
-        echo -n '------------------'
-    done
-    printf "\n\n"
-fi
-
-############ all results
-echo "Global measures"
-printf "Execution time    : %15d us\n" $duration
-
-printf 'Energy cpu        : %15d uj\n' $enegypkg
-
-if [ -n "$drams" ]; then
-
-    printf 'Energy dram       : %15d uj\n' $energydram
-
-fi
-
-if [ -n "$cores" ]; then
-
-    printf 'Energy core-cpu   : %15d uj\n' $energycore
-
-fi
-if [ -n "$uncores" ]; then
-
-    printf 'Energy uncore-cpu : %15d uj\n' $energyuncore
-
+    print_details $energies
 fi
